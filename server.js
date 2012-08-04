6b7534246e656964d260baa096a30117130295b4
Mark Woodall <mawoodall@gmail.com>
2012-08-04 12:36:33 -0400

    change server to server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.get('/data', function(req, res){
+    res.send(getStatsInfoJson());
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(process.env.PORT);
+
+function getStatsInfoJson() {
+    return {
+        "data" : [232, 242, 32, 342, 100, 97]
+    };
+}
