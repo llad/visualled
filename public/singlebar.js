c59f04fd3dd646aa8b59ba62783032419f6e57ab
Mark Woodall <mawoodall@gmail.com>
2012-08-04 11:55:41 -0400

    chart data sourced from dataserver

--- public/singlebar.js
+++ public/singlebar.js
@@ -1,14 +1,18 @@
-var data = [4, 8, 15, 16, 23, 42];
-var chart = d3.select("body")
-    .append("div")
-    .attr("class", "chart");
+//var data = [4, 8, 15, 16, 23, 42];
 
-var x = d3.scale.linear()
-    .domain([0, d3.max(data)])
-    .range(["0px", "420px"]);
 
-chart.selectAll("div")
-.data(data)
-.enter().append("div")
-.style("width", x)
-.text(String);
+d3.json("data", function(json) {
+    var data = json.data;
+
+    var chart = d3.select("body").append("div").attr("class", "chart");
+
+    var x = d3.scale.linear().domain([0, d3.max(data)]).range(["0px", "420px"]);
+
+    chart.selectAll("div")
+        .data(data)
+        .enter().append("div")
+        .style("width", x)
+        .text(String);
+});
+
+
