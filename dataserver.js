9af6175b0f37fc9c296c1b9707c7f351a6eebc33
Mark Woodall <mawoodall@gmail.com>
2012-08-03 23:27:12 -0400

    start of a dataserver with express

---
+++ dataserver.js
@@ -0,0 +1,19 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(getStatsInfoJson()));
+}).listen(process.env.PORT, process.env.IP);
+console.log('Server running at ' + process.env.PORT + ' ' + process.env.IP);
+
+
+
+function getStatsInfoJson() {
+    return {
+        "statinfo": {
+            "type":"bar",
+            "category":"parcelslast6",
+            "data" : [232, 242, 32, 342, 100, 98]
+        }
+    };
+}
---
+++ singlebar.js
@@ -0,0 +1,21 @@
+//var data = [4, 8, 15, 16, 23, 42];
+
+var data;
+
+d3.json("data", function(json) {
+  data = json.data;
+});
+
+var chart = d3.select("body")
+    .append("div")
+    .attr("class", "chart");
+
+var x = d3.scale.linear()
+    .domain([0, d3.max(data)])
+    .range(["0px", "420px"]);
+
+chart.selectAll("div")
+.data(data)
+.enter().append("div")
+.style("width", x)
+.text(String);
